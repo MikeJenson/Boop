132427b3deb8f4d576a77018979069d330557b6b
Mikel Jenson <mikeljenson@live.com>
2020-02-01 16:40:59 -0800

    4:40 Upload
    OnCollision is now working and player will disapear

---
+++ RepairGGJ/Assets/playerHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour
+{
+    public int health = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Text");
+            health -= 1;
+
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        if (other.gameObject.CompareTag("Grass"))
+        {
+
+        }
+    }
+}
